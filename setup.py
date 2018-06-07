c427621eb6ab7bcb1a0cccda5fb5a525395ae84e
Julian Zimmermann <julianzimmermann@gmail.com>
2018-06-07 20:38:58 +0200

    Reset version

--- setup.py
+++ setup.py
@@ -4,16 +4,12 @@ from os import path
 
 here = path.abspath(path.dirname(__file__))
 
-# Get the long description from the README file
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-# Arguments marked as "Required" below must be included for upload to PyPI.
-# Fields marked as "Optional" may be commented out.
-
 setup(
     name='docker-cmd',
-    version='0.2.0',
+    version='0.4.0',
     description='A builder for docker command strings',
     long_description=long_description,
     long_description_content_type='text/markdown',
